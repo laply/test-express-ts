807d158d4d6380e524fc99cf3bd334b6ac3812a9
sypark <yspark2@yookgak.com>
2023-02-14 12:13:36 +0900

    feat: express Base

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("hello world!!");
+import * as express from "express";
+
+const app: express.Express = express();
+const port: number = 8800;
+
+app.get("/", (_: express.Request, res: express.Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
